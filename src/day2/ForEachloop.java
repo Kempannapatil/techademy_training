65cd4702384d9d329c599959db8fbbcb62e7d126
darsh <darsh@Dell_123>
2025-07-22 23:02:35 +0530

    comment push

---
+++ src/day2/ForEachloop.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class ForEachloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stubt
+
+		
+		String[] sports= {"cricket","tennis","badmitton","boxing"};
+		for(String i:sports) {
+			System.out.println(i);
+		}
+	}
+
+}
---
+++ src/day2/Nestedloop.java
@@ -0,0 +1,15 @@
+package day2;
+
+public class Nestedloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    for(int i=0;i<3;i++) {
+    	System.out.println("outerloop "+ i);
+    	for(int j=0;j<2;j++) {
+    		System.out.println("innerloop "+j);
+    	}
+    }
+	}
+
+}
